a04c346eeca04095a0759c8d98a67ef3a084bb07
Saman Tofighian <samantofighian78@gmail.com>
2026-06-07 20:38:46 +0330

    update header

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { FaHamburger } from 'react-icons/fa';
 
 export default function Header() {
   const Links = [
@@ -31,11 +32,18 @@ export default function Header() {
   ];
   return (
     <header className='grid grid-cols-12 px-[6%] py-3 w-full h-14'>
-      <figure className='flex items-center col-span-2 h-full'>
+      <figure className='flex items-center order-1 col-span-3 lg:col-span-2 h-full'>
         <Image src='/img/logo.png' alt='logo' width={54} height={56} />
       </figure>
-      <nav className='col-span-7 h-full'>
-        <ul className='flex justify-center items-center gap-x-10 w-full h-full'>
+      <nav className='order-3 col-span-3 lg:col-span-7 h-full'>
+        <span className='lg:hidden flex justify-end items-center h-full'>
+          <FaHamburger
+            size='2.1rem'
+            className='cursor-pointer'
+            color='#504F4F'
+          />
+        </span>
+        <ul className='hidden lg:flex justify-center items-center gap-x-10 w-full h-full'>
           {Links.map((link) => (
             <li key={link.id}>
               <Link
@@ -52,7 +60,14 @@ export default function Header() {
           ))}
         </ul>
       </nav>
-      <div className='col-span-3 h-full'></div>
+      <div className='flex justify-center items-center gap-x-8 order-2 col-span-6 lg:col-span-3 h-full'>
+        <button className='bg-white hover:bg-[#C90000] px-5 py-2 border border-[#C90000] rounded-[100px] text-[#C90000] hover:text-white duration-700 cursor-pointer'>
+          Sign in
+        </button>
+        <button className='bg-[#C90000] hover:bg-white px-5 py-2 border border-[#C90000] rounded-[100px] text-white hover:text-[#C90000] duration-700 cursor-pointer'>
+          Logn in
+        </button>
+      </div>
     </header>
   );
 }
