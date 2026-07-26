2b478016ac524bdd54188acaa6a23cd8120bf589
Saman Tofighian <samantofighian78@gmail.com>
2026-07-26 17:58:29 +0330

    responsive  motto section

--- Components/Motto.jsx
+++ Components/Motto.jsx
@@ -11,22 +11,22 @@ export default function Motto() {
 
   return (
     <section className='mt-10 w-full'>
-      <div className='items-center gap-x-6 grid grid-cols-12 w-full'>
-        <div className='col-span-5 col-start-2'>
-          <h1 className='w-[70%] font-bold text-[#C90000] text-[48px] leading-tight'>
+      <div className='items-center gap-x-6 grid grid-cols-12 w-full px-[6%] lg:px-0 gap-y-10 xl:gap-y-0'>
+        <div className='col-span-12 xl:col-span-5 xl:col-start-2 flex flex-col items-center xl:items-start'>
+          <h1 className='text-center xl:text-left xl:w-[70%] font-bold text-[#C90000] text-[48px] leading-tight'>
             Order your favourite food
           </h1>
-          <p className='mt-4 max-w-sm text-[#504F4F]'>
+          <p className='mt-4 xl:text-left text-center xl:max-w-sm text-[#504F4F] leading-8'>
             Hamburger has many flavors, have you tried Cheese Beef Hamburger?
           </p>
 
-          <span className='block mt-6 text-[#504F4F] text-sm'>
+          <span className='block mt-9 text-[#504F4F] text-sm'>
             Total Order :{' '}
             <b className='text-[#1D1D1D] text-[18px]'>{totalPrice}</b>
           </span>
 
-          <div className='flex items-center gap-x-4 mt-4'>
-            <div className='flex items-center gap-x-4 bg-white px-4 py-3 border border-[#E5E5E5] rounded-full'>
+          <div className='flex items-center gap-x-7 mt-4'>
+            <div className='flex items-center gap-x-4 bg-white px-4 py-1.5 shadow-md border border-[#E5E5E5] rounded-full'>
               <button
                 onClick={() => setQuantity((q) => Math.min(q + 1, 99))}
                 className='text-[#7D7C7C] hover:text-[#C90000] cursor-pointer'
@@ -34,9 +34,11 @@ export default function Motto() {
               >
                 <HiPlus size={16} />
               </button>
-              <span className='min-w-[1ch] font-semibold text-[#F5A623] text-sm text-center'>
+              <span className='text-[#F1F1F1]'> | </span>
+              <span className='min-w-[1ch] font-500 font-medium text-[#FFC300] text-[20px] text-center'>
                 {quantity}
               </span>
+              <span className='text-[#F1F1F1]'> | </span>
               <button
                 onClick={() => setQuantity((q) => Math.max(q - 1, 1))}
                 className='text-[#7D7C7C] hover:text-[#C90000] cursor-pointer'
@@ -45,14 +47,14 @@ export default function Motto() {
                 <HiMinus size={16} />
               </button>
             </div>
-            <button className='flex items-center gap-x-2 bg-[#F5A623] hover:bg-[#e0951a] shadow-lg px-6 py-3 rounded-full font-semibold text-white text-sm duration-300 cursor-pointer'>
+            <button className='flex items-center gap-x-2 bg-[#FFC300] hover:bg-[#e0951a] shadow-lg px-7.5 py-4 rounded-full font-semibold text-[#323232] text-sm duration-300 cursor-pointer'>
               <HiShoppingBag size={18} />
               Order now
             </button>
           </div>
         </div>
 
-        <figure className='relative col-span-6'>
+        <figure className='relative col-span-10 flex col-start-2 justify-center xl:col-span-6'>
           <div className='top-35 -left-10 z-10 absolute flex items-center gap-x-2 bg-[#F1F1F1] shadow-sm px-4 py-2 rounded-full'>
             <FaClock className='text-[#1D1D1D]' size={14} />
             <div className='leading-tight'>
